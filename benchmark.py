59c2756812c2afc4dbd9f009df178fb2f7b877f5
Djames16 <djames@brooklynsteamcenter.org>
2023-09-19 10:50:31 -0400

    python again

--- benchmark.py
+++ benchmark.py
@@ -19,8 +19,26 @@ print("im going to break")
 
 #Excerise 5: create a list of strings called"fav_food"
 #put 3 of your favorite food items inside of the list
-fav_food = ["mac and cheese, burgers, fried chicken"]
+fav_food = ['mac and cheese', 'burgers', 'fried chicken']
 print(fav_food)
 
+#excerise 6:
+if age>=21 :
+    print ('You can rent a car')
+else : 
+    print('Your beat')
 
+#excerise 7:
+Their_Name = input("What is your name?")
+print("Hello," + Their_Name)
 
+#excerise 8:
+
+for i in fav_food :
+    print(i)
+
+#excerise 9:
+
+x=1
+while x ==2 :
+    print("equal >:)")
