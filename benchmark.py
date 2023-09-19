d13b7b7409485006105dff0029ad498f309b85e4
Djames16 <djames@brooklynsteamcenter.org>
2023-09-19 09:51:09 -0400

    python

---
+++ benchmark.py
@@ -0,0 +1,26 @@
+# Excercise 1:Display your name in the console
+
+print("Daeon James")
+
+# Excercise 2: Create a variable called FavoriteShow
+# and set it equal to the name of your favorite show
+
+FavoriteShow= ("South park")
+print("FavoriteShow")
+
+# Excercise 3: create two variables called height & # age, then display in the console, the product of them. 
+height= (6)
+Age=(17)
+print(height*Age)
+
+# Excerise 4: Display "im going to break" in the console
+
+print("im going to break")
+
+#Excerise 5: create a list of strings called"fav_food"
+#put 3 of your favorite food items inside of the list
+fav_food = ["mac and cheese, burgers, fried chicken"]
+print(fav_food)
+
+
+
